29f45807825b74f3fcc10c46a7f8d1c039150588
Leo <lshapirov2@gmail.com>
2024-09-28 18:20:27 -0500

    about class added and text modified

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,10 +11,10 @@ function About() {
             style={{ width: "100px", borderRadius: "50%", height: "130px" }} 
             />
         </div>
-        <p>
+        <p class = "about">
         I’m a full-stack software developer with a passion for building dynamic, efficient, and scalable applications. With experience across both front-end and back-end technologies, I enjoy tackling complex problems and creating solutions that offer seamless user experiences. I’m always excited to learn new tools, frameworks, and best practices to stay at the cutting edge of web development.
         </p>
-        <p>
+        <p class = "about">
         I have a master's degree in economics, which gives me a unique perspective on problem-solving, data analysis, and system optimization. With a blend of technical and analytical skills, I enjoy designing solutions that are not only functional but also efficient and impactful in a real-world context.
         </p>
       </div>
